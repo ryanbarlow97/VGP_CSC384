c4f35cac05ddc1d47ad84cb899d2822e3bf085fc
Ryan Barlow <908032@swansea.ac.uk>
2023-02-23 23:22:15 +0000

    audio aint working, removing for now

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -5,9 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void changeScene()
+    public AudioSource src;
+    
+    public AudioClip srcOne;
+    private void Awake()
     {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void changeToMainMenu()
+    {
+
+        src.PlayOneShot(srcOne);
         SceneManager.LoadScene("MainMenu");
+
+    }
+        public void changeToOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+        public void changeToLeaderboards()
+    {
+        SceneManager.LoadScene("Leaderboard");
     }
 
 }
