26628b8c045c578942eb8e8849dd4a84de93999c
Ryan Barlow <908032@swansea.ac.uk>
2023-02-24 21:00:19 +0000

    added leaderboard button control start

---
+++ Assets/Scripts/LeaderboardButtons.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LeaderboardButtons  : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("Button Hover Text:")] 
+    [SerializeField] private TMPro.TextMeshProUGUI originalText;
+
+    private Color originalColor;
+
+    private void Start()
+    {
+        originalColor = originalText.color;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        originalText.color = Color.white;
+    }
+        public void OnPointerExit(PointerEventData eventData)
+    {
+        originalText.color = originalColor;
+    }
+
+    public void Back() 
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
