f8a53f21800f9c0237637bb37d76ace91ec8ef7b
Ryan Barlow <908032@swansea.ac.uk>
2023-02-28 17:45:49 +0000

    added comments and changed code for efficiency

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,32 +4,42 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float lengthX, lengthY, startX, startY;
+    private float lengthX, lengthY;
     public Camera cam;
     public float parallaxEffectX;
     public float parallaxEffectY;
 
     void Start()
     {
-        startX = transform.position.x;
-        startY = transform.position.y;
+        // Store the starting position of the sprite
+        startPosition = transform.position;
+
+        // Get the length of the sprite on the x and y axis
         lengthX = GetComponent<SpriteRenderer>().bounds.size.x;
         lengthY = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     void Update()
     {
-        float tempX = (cam.transform.position.x * (1 - parallaxEffectX));
-        float distX = (cam.transform.position.x * parallaxEffectX);
-        float tempY = (cam.transform.position.y * (1 - parallaxEffectY));
-        float distY = (cam.transform.position.y * parallaxEffectY);
+        // Calculate the distance of the camera on the x and y axis
+        float tempX = cam.transform.position.x * (1 - parallaxEffectX);
+        float distX = cam.transform.position.x * parallaxEffectX;
+        float tempY = cam.transform.position.y * (1 - parallaxEffectY);
+        float distY = cam.transform.position.y * parallaxEffectY;
 
-        transform.position = new Vector3(startX + distX, startY + distY, transform.position.z);
+        // Update the position of the sprite based on the camera distance and parallax effect
+        transform.position = new Vector3(startPosition.x + distX, startPosition.y + distY, transform.position.z);
 
-        if (tempX > startX + lengthX) startX += lengthX;
-        else if (tempX < startX - lengthX) startX -= lengthX;
+        // If the camera has moved beyond the bounds of the sprite on the x axis, update the starting position to move the sprite with the camera
+        if (tempX > startPosition.x + lengthX) startPosition.x += lengthX;
 
-        if (tempY > startY + lengthY) startY += lengthY;
-        else if (tempY < startY - lengthY) startY -= lengthY;
+        // If the camera has moved in the opposite direction, update the starting position accordingly
+        else if (tempX < startPosition.x - lengthX) startPosition.x -= lengthX;
+
+        // If the camera has moved beyond the bounds of the sprite on the y axis, update the starting position to move the sprite with the camera
+        if (tempY > startPosition.y + lengthY) startPosition.y += lengthY;
+
+        // If the camera has moved in the opposite direction, update the starting position accordingly
+        else if (tempY < startPosition.y - lengthY) startPosition.y -= lengthY;
     }
 }
