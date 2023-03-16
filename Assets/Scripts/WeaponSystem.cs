881098939bdca7d9db5999f870c657e60d6e1296
Ryan Barlow <908032@swansea.ac.uk>
2023-03-16 18:20:39 +0000

    created bullet shooting system

--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class WeaponSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+    public Transform firePoint;
+    public float fireRate = 0.25f; // Time in seconds between shots
+    private float nextFireTime = 0f; // Time when the next shot is allowed
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Shoot();
+        }
+    }   
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
     }
 }
