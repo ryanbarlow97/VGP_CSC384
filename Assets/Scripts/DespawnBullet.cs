af20eee14216b227a46366a47cd31e6917f03ba4
Ryan Barlow <908032@swansea.ac.uk>
2023-03-16 18:20:59 +0000

    despawn bullets

---
+++ Assets/Scripts/DespawnBullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnBullet : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float minX, maxX, minY, maxY;
+    public float offset = 2f; // Add an offset value
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Vector3 screenBottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
+        Vector3 screenTopRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane));
+        minX = screenBottomLeft.x - offset;
+        maxX = screenTopRight.x + offset;
+        minY = screenBottomLeft.y - offset;
+        maxY = screenTopRight.y + offset;
+    }
+
+    void Update()
+    {
+        if (transform.position.x < minX || transform.position.x > maxX || transform.position.y < minY || transform.position.y > maxY)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
