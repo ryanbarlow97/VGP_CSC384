6a6f2f8a08e53fdf37a2ccfbb78ac1ff99b7d43d
Ryan Barlow <908032@swansea.ac.uk>
2023-03-17 02:36:15 +0000

    wrote some collision stuff for meteors

--- Assets/Scripts/MeteorMovement.cs
+++ Assets/Scripts/MeteorMovement.cs
@@ -5,14 +5,44 @@ public class MeteorMovement : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D rb;
 
+    public GameObject[] smallMeteorPrefabs;
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         
-        // Calculate direction towards the center of the screen
-        Vector3 screenCenterWorld = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane));
-        Vector2 direction = (screenCenterWorld - transform.position).normalized;
-        
+        if (rb.velocity == Vector2.zero)
+        {
+            // Calculate direction towards the center of the screen
+            Vector3 screenCenterWorld = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.nearClipPlane));
+            Vector2 direction = (screenCenterWorld - transform.position).normalized;
+            rb.velocity = direction * speed;
+        }
+    }
+    public void SpawnSmallerMeteors()
+    {
+        if (smallMeteorPrefabs != null && smallMeteorPrefabs.Length == 4)
+        {
+            Vector2[] directions = {
+                new Vector2(-1, 1).normalized,    // Top left
+                new Vector2(1, 1).normalized,     // Top right
+                new Vector2(-1, -1).normalized,   // Bottom left
+                new Vector2(1, -1).normalized     // Bottom right
+            };
+
+            for (int i = 0; i < 4; i++)
+            {
+                GameObject smallMeteor = Instantiate(smallMeteorPrefabs[i], transform.position, Quaternion.identity);
+                MeteorMovement smallMeteorMovement = smallMeteor.GetComponent<MeteorMovement>();
+                smallMeteorMovement.SetDirection(directions[i]);
+            }
+        }
+    }
+    public void SetDirection(Vector2 direction)
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
         rb.velocity = direction * speed;
     }
 }
