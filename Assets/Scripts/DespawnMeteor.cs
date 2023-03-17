d79c29b149dc7851fddfe49a5b109c3b79db854d
Ryan Barlow <908032@swansea.ac.uk>
2023-03-17 02:13:15 +0000

    changed code to check for new screen area each update

--- Assets/Scripts/DespawnMeteor.cs
+++ Assets/Scripts/DespawnMeteor.cs
@@ -21,6 +21,12 @@ public class DespawnMeteor : MonoBehaviour
 
     void Update()
     {
+        Vector3 screenBottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
+        Vector3 screenTopRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane));
+        minX = screenBottomLeft.x - offset;
+        maxX = screenTopRight.x + offset;
+        minY = screenBottomLeft.y - offset;
+        maxY = screenTopRight.y + offset;
         if (transform.position.x < minX || transform.position.x > maxX || transform.position.y < minY || transform.position.y > maxY)
         {
             Destroy(gameObject);
