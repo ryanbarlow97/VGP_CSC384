b744148f0edcf49232fab1eb0cf87dbc6893de8a
Ryan Barlow <908032@swansea.ac.uk>
2023-03-17 02:36:23 +0000

    added collision spawner for meteors

---
+++ Assets/Scripts/MeteorCollision.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MeteorCollision : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    private MeteorMovement meteorMovement;
+
+    private void Start()
+    {
+        meteorMovement = GetComponent<MeteorMovement>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            // Destroy the bullet
+            Destroy(collision.gameObject);
+
+            // Spawn an explosion at the meteor's position
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+            // Spawn smaller meteors
+            meteorMovement.SpawnSmallerMeteors();
+
+            // Destroy the meteor
+            Destroy(gameObject);
+        }
+    }
+}
