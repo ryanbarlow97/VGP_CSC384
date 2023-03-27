1302b05ac6ef82033a8d39b21971d143cc0fbff8
Ryan Barlow <908032@swansea.ac.uk>
2023-03-27 02:44:36 +0100

    despawn controller for meteors using Despawn ICommand

---
+++ Assets/Scripts/MeteorDespawnController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MeteorDespawnController : MonoBehaviour
+{
+    public float offset = 2f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ICommand despawnCommand = new DespawnCommand(gameObject, offset);
+        despawnCommand.Execute();
+    }
+}
