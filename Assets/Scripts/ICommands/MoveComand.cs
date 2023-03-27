2b2553d1c11c4320fe77896135e7841ba568deab
Ryan Barlow <908032@swansea.ac.uk>
2023-03-27 02:44:01 +0100

    refactored/reformatted and edited the way movement is called in MoveCommand

--- Assets/Scripts/ICommands/MoveComand.cs
+++ Assets/Scripts/ICommands/MoveComand.cs
@@ -7,20 +7,18 @@ public class MoveCommand : ICommand
     private Rigidbody2D rb;
     private float acceleration;
     private float maxSpeed;
-    private float vertical;
-
-    public MoveCommand(Rigidbody2D rb, float acceleration, float maxSpeed, float vertical)
+    private Vector2 direction;
+    public MoveCommand(Rigidbody2D rb, float acceleration, float maxSpeed, Vector2 direction)
     {
         this.rb = rb;
         this.acceleration = acceleration;
         this.maxSpeed = maxSpeed;
-        this.vertical = vertical;
+        this.direction = direction;
     }
 
     public void Execute()
     {
-        Vector2 direction = rb.transform.up;
-        rb.velocity += direction * acceleration * Time.deltaTime;
+        rb.velocity = direction * acceleration;
         rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
     }
 }
