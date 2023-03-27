1f213a5581c23e8973a8d389e7175a57addf3635
Ryan Barlow <908032@swansea.ac.uk>
2023-03-27 03:25:21 +0100

    set screenoffset to 0 (was 2) and added proper spawn position along top/bot/left/right and now randomly creates meteors between size 0.5f and 2f, and sets speed relative to size

--- Assets/Scripts/MeteorSpawn.cs
+++ Assets/Scripts/MeteorSpawn.cs
@@ -6,13 +6,14 @@ public class MeteorSpawn : MonoBehaviour
     public GameObject meteorPrefab;
     public float spawnRate = 1f;
     private Camera mainCamera;
-    private float screenOffset = 2f;
+    private float screenOffset = 0f;
 
     void Start()
     {
         mainCamera = Camera.main;
         StartCoroutine(SpawnMeteors());
     }
+
     IEnumerator SpawnMeteors()
     {
         while (true)
@@ -24,12 +25,10 @@ public class MeteorSpawn : MonoBehaviour
             int randomEdge = Random.Range(0, 4);
 
             // Recalculate screen boundaries each time a meteor is spawned
-            Vector3 screenBottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
-            Vector3 screenTopRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane));
-            float minX = screenBottomLeft.x + screenOffset;
-            float maxX = screenTopRight.x - screenOffset;
-            float minY = screenBottomLeft.y + screenOffset;
-            float maxY = screenTopRight.y - screenOffset;
+            float screenLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane)).x + screenOffset;
+            float screenRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, mainCamera.nearClipPlane)).x - screenOffset;
+            float screenBottom = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane)).y + screenOffset;
+            float screenTop = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, mainCamera.nearClipPlane)).y - screenOffset;
 
             // Randomly select the direction and speed of the meteor
             Vector2 meteorDirection = Vector2.zero;
@@ -39,31 +38,35 @@ public class MeteorSpawn : MonoBehaviour
                 case 0: // Top
                     meteorDirection = Vector2.down;
                     meteorSpeed = Random.Range(5f, 10f);
+                    spawnPosition = new Vector3(Random.Range(screenLeft, screenRight), screenTop, 0f);
                     break;
                 case 1: // Bottom
                     meteorDirection = Vector2.up;
                     meteorSpeed = Random.Range(5f, 10f);
+                    spawnPosition = new Vector3(Random.Range(screenLeft, screenRight), screenBottom, 0f);
                     break;
                 case 2: // Left
                     meteorDirection = Vector2.right;
                     meteorSpeed = Random.Range(5f, 10f);
+                    spawnPosition = new Vector3(screenLeft, Random.Range(screenBottom, screenTop), 0f);
                     break;
                 case 3: // Right
                     meteorDirection = Vector2.left;
                     meteorSpeed = Random.Range(5f, 10f);
+                    spawnPosition = new Vector3(screenRight, Random.Range(screenBottom, screenTop), 0f);
                     break;
             }
 
-            // Calculate the meteor's initial position and rotation
-            spawnPosition = mainCamera.ViewportToWorldPoint(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), mainCamera.nearClipPlane));
-            spawnPosition.x = Mathf.Clamp(spawnPosition.x, minX, maxX);
-            spawnPosition.y = Mathf.Clamp(spawnPosition.y, minY, maxY);
-            spawnPosition.z = 0f;
+            // Calculate the meteor's initial rotation
             spawnRotation = Quaternion.LookRotation(Vector3.forward, meteorDirection);
 
-            // Scale the meteor size based on its speed
-            float scale = Mathf.Clamp(meteorSpeed / 10f, 0.5f, 2f);
+            // Scale the meteor size based on a random range between 1 and 3
+            float scale = Random.Range(0.5f, 2f);
             meteorPrefab.transform.localScale = new Vector3(scale, scale, 1);
+
+            // Set the meteor speed based on the size
+            meteorSpeed = Random.Range(5f, 10f) * scale;
+
 
             // Instantiate the meteor and set its initial velocity
             GameObject meteor = Instantiate(meteorPrefab, spawnPosition, spawnRotation);
