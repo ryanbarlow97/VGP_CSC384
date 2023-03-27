b6b544b2e25e78e25caca85d94cfa75c7fa1e4eb
Ryan Barlow <908032@swansea.ac.uk>
2023-03-27 02:41:33 +0100

    add reference to spawning small meterors via ICommand now

--- Assets/Scripts/MeteorCollision.cs
+++ Assets/Scripts/MeteorCollision.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MeteorCollision : MonoBehaviour
 {
     public GameObject explosionPrefab;
+    public GameObject[] smallMeteorPrefabs;
 
     private MeteorMovement meteorMovement;
 
@@ -21,13 +22,21 @@ public class MeteorCollision : MonoBehaviour
             // Spawn an explosion at the meteor's position
             GameObject newExplosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
 
+            // Get the bullet impact direction
+            Vector2 bulletImpactDirection = collision.GetContact(0).normal * -1;
+
             // Spawn smaller meteors
-            meteorMovement.SpawnSmallerMeteors();
+            ICommand spawnSmallerMeteorsCommand = new SpawnSmallerMeteorsCommand(meteorMovement.smallMeteorPrefabs, transform.position, meteorMovement.speed / 2, -collision.contacts[0].normal);
+
+            
+            spawnSmallerMeteorsCommand.Execute();
 
             // Destroy the meteor
             Destroy(gameObject);
+
             //Destroy explosion after 1.25 seconds
             Destroy(newExplosion, 1.25f);
         }
     }
+
 }
