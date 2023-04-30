4f116acc08e70e9a46f5559ae9da0453f2c94756
Ryan Barlow <Ryanbarlow123@me.com>
2023-04-30 18:30:16 +0100

    changed to velocity not addforce because using kinematic now

--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -19,11 +19,10 @@ public class WeaponSystem : MonoBehaviour
             Shoot();
         }
     }   
-
     void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+        rb.velocity = firePoint.up * bulletSpeed;
     }
 }
