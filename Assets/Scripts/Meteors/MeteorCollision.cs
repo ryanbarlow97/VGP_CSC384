7a69a6336559875d83f49b04e2aef01dec956c8e
Ryan Barlow <Ryanbarlow123@me.com>
2023-04-30 18:28:05 +0100

    convert to trigger

--- Assets/Scripts/Meteors/MeteorCollision.cs
+++ Assets/Scripts/Meteors/MeteorCollision.cs
@@ -5,25 +5,25 @@ public class MeteorCollision : MonoBehaviour
     public GameObject explosionPrefab;
     public GameObject[] smallMeteorPrefabs;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.collider.CompareTag("Bullet"))
+        if (other.CompareTag("Bullet"))
         {
             // Destroy the bullet
-            Destroy(collision.gameObject);
+            Destroy(other.gameObject);
 
             // Spawn an explosion at the meteor's position
             GameObject newExplosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
 
             // Get the bullet impact direction
-            Vector2 bulletImpactDirection = collision.GetContact(0).normal * -1;
+            Vector2 bulletImpactDirection = (other.transform.position - transform.position).normalized;
 
             // Get the meteor's speed at the time of collision
             float meteorSpeed = GetComponent<Rigidbody2D>().velocity.magnitude;
 
             // Spawn smaller meteors
             ICommand spawnSmallerMeteorsCommand = new SpawnSmallerMeteorsCommand(
-                smallMeteorPrefabs, transform.position, meteorSpeed / 2, -collision.contacts[0].normal, transform.localScale.x);
+                smallMeteorPrefabs, transform.position, meteorSpeed / 2, bulletImpactDirection, transform.localScale.x);
             spawnSmallerMeteorsCommand.Execute();
 
             // Destroy the meteor
@@ -33,7 +33,7 @@ public class MeteorCollision : MonoBehaviour
             Destroy(newExplosion, 0.8f);
         }
 
-        if (collision.collider.CompareTag("PlayerShip"))
+        if (other.CompareTag("PlayerShip"))
         {
             Debug.Log("PlayerLost");
         }
